838c96690ccf5646a1990162b09fa7cfb9031d75
areejsiddique <125815158+areejsiddique@users.noreply.github.com>
2023-10-14 08:45:40 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// CHAPTER # 01
+// Practice exercise 1.1
+
+// working on console.
+
+// Practice exercise 1.2
+
+// make a script tag in direct html (internal linking)
+
+// Practice exercise 1.3
+
+alert("hello there!");
+console.log("its me AREEJ");
+
+// Practice exercise 1.4
+
+  var abc = 20; // setting the value of 20 in variable abc
+  console.log(abc); // console.log show the value store in variable in web page
+  
+  /* we can comment in javascript by 2 method
+  one is one-line comment 
+  and other is multiple line comment 
+  here i am using multiple line comment */
+
+
+
+  //Chapter project
+
+  //console.log("AreejSiddique");
+
+ // Self-check quiz
+
+ // 1.<script src="./app.js"></script>
+ // 2. yes
+ // 3. /* ---------- */
+ // 4. by comment out the line 
